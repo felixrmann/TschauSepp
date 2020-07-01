c079b5dd80a87fe6cc3d6ac0ae8a4af537091201
Felix <felixmann00@gmail.com>
2020-07-01 14:06:56 +0200

    -

--- src/TschauSepp/view/Menue.java
+++ src/TschauSepp/view/Menue.java
@@ -4,6 +4,7 @@ import TschauSepp.controller.MenueController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 /**
  * @author Felix Mann
@@ -13,14 +14,13 @@ import java.awt.*;
 
 public class Menue extends JPanel{
     private MainFrame mainFrame;
-    private JPanel mainPanel, mainmainPanel, topPanel, botPanel, botMidPanel;
+    private JPanel mainPanel, topPanel, botPanel, botMidPanel;
     private JButton playButton, regelnButton, exitButton;
 
     public Menue(MainFrame mainFrame){
         this.mainFrame = mainFrame;
 
         mainPanel = new JPanel();
-        mainmainPanel = new JPanel();
         topPanel = new JPanel();
         botPanel = new JPanel();
         botMidPanel = new JPanel();
@@ -36,19 +36,35 @@ public class Menue extends JPanel{
     private void init(){
         add(mainPanel);
 
-        mainPanel.setLayout(new BorderLayout(10,10));
-        mainPanel.add(mainmainPanel);
+        mainPanel.setBackground(new Color(30,87,216));
+        topPanel.setBackground(new Color(30,87,216));
+        botMidPanel.setBackground(new Color(30,87,216));
+        botPanel.setBackground(new Color(30,87,216));
 
-        mainmainPanel.setLayout(new GridLayout(2,1));
-        mainmainPanel.add(topPanel);
-        mainmainPanel.add(botPanel);
+        mainPanel.setLayout(new GridLayout(2,1));
+        mainPanel.setPreferredSize(new Dimension(400,458));
+        mainPanel.add(topPanel);
+        mainPanel.add(botPanel);
+
+        URL path1 = this.getClass().getResource("../img/titleimg.png");
+        ImageIcon imageIcon1 = new ImageIcon(path1);
+        Image image1 = imageIcon1.getImage();
+        Image newimg1 = image1.getScaledInstance(400, 200, java.awt.Image.SCALE_SMOOTH);
+        imageIcon1 = new ImageIcon(newimg1);
+        JLabel img1 = new JLabel();
+        img1.setIcon(imageIcon1);
+        topPanel.add(img1);
 
         //TODO topPanel (Titel oder sowas)
 
         botPanel.setLayout(new GridLayout(1, 3));
-        botPanel.add(new JPanel());
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(new Color(30,87,216));
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(new Color(30,87,216));
+        botPanel.add(panel1);
         botPanel.add(botMidPanel);
-        botPanel.add(new JPanel());
+        botPanel.add(panel2);
 
         GridLayout grid = new GridLayout(3,1);
         grid.setVgap(50);
